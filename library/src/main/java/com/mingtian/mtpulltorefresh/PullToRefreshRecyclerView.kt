84a748c71d9ba21f6db85f93645c5c0b6b2c70a3
TomorrowX <xmt5814122@gmail.com>
2018-05-07 12:16:31 +0800

    Initial commit

---
+++ library/src/main/java/com/mingtian/mtpulltorefresh/PullToRefreshRecyclerView.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2017. Beijing CloudHua Science and Technology Co., Ltd.. All rights reserved.
+ */
+
+package com.mingtian.mtpulltorefresh
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.util.AttributeSet
+import com.handmark.pulltorefresh.library.R
+
+/**
+ * PullToRefreshRecyclerView.java
+ * Created by xiemingtian on 2017/10/19.
+ */
+
+class PullToRefreshRecyclerView : PullToRefreshBase<RecyclerView> {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, mode: Mode) : super(context, mode)
+
+    constructor(context: Context, mode: Mode, style: AnimationStyle) : super(context, mode, style) {}
+
+    override fun getPullToRefreshScrollDirection(): Orientation =
+            Orientation.VERTICAL
+
+    override fun createRefreshableView(context: Context, attrs: AttributeSet): RecyclerView {
+        val recyclerView: RecyclerView
+        recyclerView = InternalScrollViewSDK9(context, attrs)
+        recyclerView.setId(R.id.recyclerview)
+        return recyclerView
+    }
+
+    override fun isReadyForPullStart(): Boolean {
+        val layoutManager = mRefreshableView.layoutManager
+        if (layoutManager is LinearLayoutManager) {
+            return if (layoutManager.orientation == LinearLayoutManager.VERTICAL) {
+                0 == layoutManager.findFirstCompletelyVisibleItemPosition()
+            } else {
+                false
+            }
+        } else if (layoutManager is StaggeredGridLayoutManager) {
+            return if (layoutManager.orientation == LinearLayoutManager.VERTICAL) {
+                val ints = layoutManager.findFirstCompletelyVisibleItemPositions(null)
+                if (mRefreshableView.childCount > 0) {
+                    val firstView = mRefreshableView.getChildAt(0)
+                    (firstView.y == 0f).and(ints[0] == 0)
+                } else {
+                    true
+                }
+            } else {
+                false
+            }
+        }
+        return mRefreshableView.scrollY == 0
+    }
+
+    override fun isReadyForPullEnd(): Boolean {
+        val scrollViewChild = mRefreshableView.getChildAt(0)
+        return if (null != scrollViewChild) {
+            mRefreshableView.scrollY >= scrollViewChild.height - height
+        } else false
+    }
+
+    @TargetApi(9)
+    internal inner class InternalScrollViewSDK9(context: Context, attrs: AttributeSet) : RecyclerView(context, attrs) {
+
+        /**
+         * Taken from the AOSP ScrollView source
+         */
+        private val scrollRange: Int
+            get() {
+                var scrollRange = 0
+                if (childCount > 0) {
+                    val child = getChildAt(0)
+                    scrollRange = Math.max(0, child.height - (height - paddingBottom - paddingTop))
+                }
+                return scrollRange
+            }
+
+        override fun overScrollBy(deltaX: Int, deltaY: Int, scrollX: Int, scrollY: Int, scrollRangeX: Int,
+                                  scrollRangeY: Int, maxOverScrollX: Int, maxOverScrollY: Int, isTouchEvent: Boolean): Boolean {
+
+            val returnValue = super.overScrollBy(deltaX, deltaY, scrollX, scrollY, scrollRangeX,
+                    scrollRangeY, maxOverScrollX, maxOverScrollY, isTouchEvent)
+
+            // Does all of the hard work...
+            OverscrollHelper.overScrollBy(this@PullToRefreshRecyclerView, deltaX, scrollX, deltaY, scrollY,
+                    scrollRange, isTouchEvent)
+
+            return returnValue
+        }
+    }
+}
